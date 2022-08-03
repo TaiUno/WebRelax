4156bc73315a3eb7c7f5021b93768388eba2f26b
TaiUno <phamthanhtai236@gmail.com>
2022-08-04 00:45:53 +0700

    Modifed UI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="h-screen bg-[#0d1017] px-4 py-6 relative">
-      <div className="absolute bg-650 after:content-none inset-0 bg-gradient-radial">
+    <div className="h-screen bg-[#0d1017] relative overflow-hidden">
+      <div
+        className="absolute bg-650 after:content-[' '] inset-0 -bottom-[650px] bg-gradient-radial
+      animate-spin-slow"
+      ></div>
+      <div className="w-full absolute flex-grow justify-center">
         <div className="mt-28 text-center text-white text-5xl font-sans">
           <h1>Put a stressful thought in the star</h1>
         </div>
         <div className="flex items-center justify-center mt-6 bg-gradient-radial2">
-          <div className="flex items-center justify-center w-72 h-72 bg-white rounded-full shadow-3xl"></div>
+          <div className="flex items-center justify-center w-72 h-72 bg-uyen-em bg-cover rounded-full shadow-3xl"></div>
         </div>
         <div className="flex items-center justify-center mt-6 w-full">
           <input
@@ -17,10 +21,7 @@ function App() {
           ></input>
         </div>
         <div className="flex items-center justify-center mt-6 w-full">
-          <button
-            className=" rounded-2xl h-11 w-28 border-none bg-[#a7582c] font-normal text-white hover:text-[#a7582c] hover:bg-white"
-            text
-          >
+          <button className=" rounded-2xl h-11 w-28 border-none bg-[#a7582c] font-semibold text-white hover:text-[#a7582c] hover:bg-white">
             DONE
           </button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,31 @@ module.exports = {
       },
       backgroundImage: {
         "gradient-radial":
-          "radial-gradient(4px 4px at 100px 50px, #fff, transparent), radial-gradient(6px 6px at 200px 150px, #fff, transparent), radial-gradient(3px 3px at 300px 250px, #fff, transparent), radial-gradient(4px 4px at 400px 350px, #fff, transparent),radial-gradient(6px 6px at 500px 100px, #fff, transparent),radial-gradient(3px 3px at 50px 200px, #fff, transparent),radial-gradient(4px 4px at 150px 300px, #fff, transparent),radial-gradient(6px 6px at 250px 400px, #fff, transparent),radial-gradient(4px 4px at 350px 500px, #fff, transparent)",
+          "radial-gradient(4px 4px at 100px 50px, #fff, transparent), radial-gradient(6px 6px at 200px 150px, #fff, transparent), radial-gradient(3px 3px at 300px 250px, #fff, transparent), radial-gradient(4px 4px at 400px 350px, #fff, transparent),radial-gradient(6px 6px at 500px 100px, #fff, transparent),radial-gradient(3px 3px at 50px 200px, #fff, transparent),radial-gradient(4px 4px at 150px 300px, #fff, transparent),radial-gradient(6px 6px at 250px 400px, #fff, transparent),radial-gradient(4px 4px at 350px 500px, #fff, transparent),radial-gradient(3px 3px at 550px 550px, #fff, transparent),radial-gradient(0.5px 0.5px at 10px 50px, #fff, transparent),radial-gradient(0.5px 0.5px at 600px 100px, #fff, transparent),radial-gradient(1.5px 1.5px at 100px 500px, #fff, transparent)",
+        "uyen-em": "url('../src/img/em_02.jpg')",
       },
       backgroundSize: {
         auto: "auto",
         cover: "cover",
         contain: "contain",
         650: "650px 650px",
+      },
+      blur: {
+        xs: "2px",
+        is: "1px",
+      },
+      animation: {
+        "spin-slow": "spin 4s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          from: {
+            transform: "translateY(0)",
+          },
+          to: {
+            transform: "translateY(-650px)",
+          },
+        },
       },
     },
   },
